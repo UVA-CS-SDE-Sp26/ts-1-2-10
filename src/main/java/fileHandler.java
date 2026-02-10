858e788d9abd7b9872fd7a949281bdbc0f63ac2f
pelin-sayar <pelinsayar14@gmail.com>
2026-02-10 14:53:10 -0500

    initial commit for fileHandler.java

---
+++ src/main/java/fileHandler.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class fileHandler {
+
+    public String[] readFile(){
+        return null;
+    }
+
+    public String readFile(String file){
+        return null;
+    }
+
+    public String readFile(String file, String key){
+        return null;
+    }
+}
