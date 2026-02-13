e5ebb0b2189a5db315835612aaf91dab29625724
pelin-sayar <pelinsayar14@gmail.com>
2026-02-12 23:29:16 -0500

    wrote tests and fixed documentation

--- src/main/java/FileHandler.java
+++ src/main/java/FileHandler.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-public class fileHandler {
+public class FileHandler {
 
     public String[] readFile(){
         //returns String aray of all files in the data directory by name
@@ -15,7 +15,7 @@ public class fileHandler {
     }
 
     public String readFile(String fileName) throws FileNotFoundException{
-        //returns the contents of the specified file using regular decrypting key
+        //returns the contents of the specified file
         String path = "./data/" + fileName;
         String contents = "";
 
@@ -26,25 +26,11 @@ public class fileHandler {
             contents += readIn.nextLine() + "\n";
         }
 
-        readIn.close();
-
-        return contents;
-    }
-
-    public String readFile(String fileName, String key) throws FileNotFoundException{
-        //returns the contents of the specified files using unique decrypting key
-        String path = "./data/" + fileName;
-        String contents = "";
-
-        File file = new File(path);
-        Scanner readIn = new Scanner(file);
-
-        while(readIn.hasNextLine()){
-            contents += readIn.nextLine() + "\n";
+        if (!contents.equals("")){
+            contents = contents.substring(0, contents.length()-1);
         }
 
         readIn.close();
-
 
         return contents;
     }
