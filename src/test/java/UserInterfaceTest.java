4f5c743cd509fcbbad253cde3378aa0f04a1db70
DennisNi08 <cgh7bv@virginia.edu>
2026-02-13 14:35:03 -0500

    fixed user interface docs

--- src/test/java/UserInterfaceTest.java
+++ src/test/java/UserInterfaceTest.java
@@ -1,16 +1,28 @@
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+
+import static org.mockito.Mockito.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class UserInterfaceTest {
-    private static UserInterface testInterface;
+@ExtendWith(MockitoExtension.class)
+class UserInterfaceTest{
+    @Mock
+        private UserInterface testInterface;
+    @Mock
+        private ProgramControl programControl;
+
     @BeforeEach
     public void initializeUI(){
-        String[] userInput = new String[]{"01","abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"};
+        String[] userInput = {"01","abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"};
         testInterface = new UserInterface(userInput);
+        testInterface.setProgramControl(programControl);
     }
 
     @Test
@@ -36,26 +48,25 @@ class UserInterfaceTest {
 
     @Test
     void transferFileSelected() throws FileNotFoundException {
-        String content = "Carnivore, later renamed DCS1000, was a system implemented by the Federal Bureau of Investigation (FBI) that was\n" +
-                "designed to monitor email and electronic communications. It used a customizable packet sniffer that could monitor all\n" +
-                "of a target user's Internet traffic. Carnivore was implemented in October 1997. By 2005 it had been replaced with\n" +
-                "improved commercial software.";
-        assertEquals(content, testInterface.transferFileSelected());
+        when(programControl.fetchFile(testInterface.getFileSelected())).thenReturn("File content");
+        assertEquals("File content", testInterface.transferFileSelected());
+        verify(programControl).fetchFile(testInterface.getFileSelected());
     }
 
     @Test
     void transferFileSelectedWithAltKey() throws FileNotFoundException {
-        String content = "Dbsojwpsf, mbufs sfobnfe EDT2aaa, xbt b tztufn jnqmfnfoufe cz uif Gfefsbm Cvsfbv pg Jowftujhbujpo (GCJ) uibu xbt\n" +
-                "eftjhofe up npojups fnbjm boe fmfduspojd dpnnvojdbujpot. Ju vtfe b dvtupnjAbcmf qbdlfu tojggfs uibu dpvme npojups bmm\n" +
-                "pg b ubshfu vtfs't Joufsofu usbggjd. Dbsojwpsf xbt jnqmfnfoufe jo Pdupcfs 2008. Cz 3aa6 ju ibe cffo sfqmbdfe xjui\n" +
-                "jnqspwfe dpnnfsdjbm tpguxbsf.";
-        assertEquals(content, testInterface.transferFileSelectedWithAltKey());
+        when(programControl.fetchFile(testInterface.getFileSelected(),testInterface.getAltKey())).thenReturn("Content");
+        assertEquals("Content", testInterface.transferFileSelectedWithAltKey());
+        verify(programControl).fetchFile(testInterface.getFileSelected(),testInterface.getAltKey());
+
     }
 
     @Test
     void transfer() {
-        String[] file = {"carnivore.cip", "carnivore.txt", "cointelpro.cip", "cointelpro.txt"};
-        assertArrayEquals(file, testInterface.transfer(), "Name of files should be the same as those in the directory \"data/\".");
+        String[] expected = new String[]{"list"};
+        when(programControl.fetchFile()).thenReturn(expected);
+        assertEquals(expected, testInterface.transfer());
+        verify(programControl).fetchFile();
     }
 
 }
